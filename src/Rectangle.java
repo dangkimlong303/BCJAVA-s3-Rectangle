9c9da591bf276a06a654eb6b7114da6d3084127d
dangkimlong303 <dangkimlong303@gmail.com>
2018-10-31 09:14:53 +0700

    rectangle

---
+++ src/Rectangle.java
@@ -0,0 +1,23 @@
+public class Rectangle {
+    private double width;
+    private double height;
+
+    public Rectangle (){
+
+    }
+    public Rectangle(double width,double height){
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getArea(){
+        return this.width * this.height;
+    }
+
+    public double getPerimeter(){
+        return (this.height + this.width)*2;
+    }
+    public String display(){
+        return "Rectangle{" + "width= " + this.width + " height= " + height + "}";
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(3,4);
+        System.out.println("Area: " + rectangle.getArea());
+        System.out.println("Perimeter: " + rectangle.getPerimeter());
+        System.out.println(rectangle.display());
+    }
+}
